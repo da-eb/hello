2d1d1e71bca3e9f8c68d804fd7d0e4e2a66d2a95
da-eb <davidlaebube@gmail.com>
2022-04-06 10:22:22 +0100

    1

---
+++ hello.py
@@ -0,0 +1,9 @@
+class Hello():
+    
+    def hello():
+        hello = "Hello, World!"
+        
+        return print(hello)
+
+
+Hello.hello()
---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+import "rsc.io/quote"
+
+// Hello world
+func main() {
+    fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+
+}
